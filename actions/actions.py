de6c7c79273dc667fe3828482f9793199a8c4236
MitanshiKshatriya <mitanshikshatriya@gmail.com>
2020-12-08 00:11:30 +0530

    actions working

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,10 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +25,17 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionSideEffects(Action):
+
+    def name(self) -> Text:
+        return "action_side_effects"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="action is running!!")
+
+        return []
+
